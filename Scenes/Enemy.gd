723a2f6feab42ac797e0467435aa931d158ab9e3
Lanilor53 <lanilor53@gmail.com>
2022-05-06 18:13:26 +0300

    add basic player health/enemy damage system

--- Scenes/Enemy.gd
+++ Scenes/Enemy.gd
@@ -1,17 +1,23 @@
 extends KinematicBody2D
 
 var player
-var SPEED = 300
-var DETECT_DISTANCE = 150
+export var SPEED = 100
+export var DETECT_DISTANCE = 150
+export var CONTACT_DAMAGE = 10
 
 func _ready():
 	player = get_tree().get_root().get_node("Main").get_node("Player")
+	set_meta("damageable", true)
 	
 func _process(delta):
 	var player_distance = self.position.distance_to(player.position)
-	print(player_distance)
 	if player_distance <= DETECT_DISTANCE:
 		var velocity = self.position.direction_to(player.position)
 		if velocity.length() > 0:
 			velocity = velocity.normalized() * SPEED
 		move_and_slide(velocity)
+		
+		for i in range(get_slide_count()-1):
+			var collision = get_slide_collision(i)
+			if collision.collider.has_meta("damageable") and collision.collider.get_meta("damageable") == true:
+				collision.collider.on_damage(self, CONTACT_DAMAGE)
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,17 +1,18 @@
 extends KinematicBody2D
 
-export var speed = 400
+export var SPEED = 400
 var screen_size
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+onready var health = 100
+export var MAX_INVINCIBILITY_TIME = 1
+onready var current_invincibility_time = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_meta("damageable", true)
 
 func _process(delta):
+	if current_invincibility_time > 0:
+		current_invincibility_time -= delta
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		 velocity.x += 1
@@ -23,11 +24,18 @@ func _process(delta):
 		velocity.y -= 1
 	
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-    
+		velocity = velocity.normalized() * SPEED
+	
 		move_and_slide(velocity)
 	
-
+func on_damage(source, damage):
+	if current_invincibility_time <= 0:
+		health -= damage
+		print("taken damage, health left: %d" % health)
+		if health <= 0:
+			queue_free() # die
+		current_invincibility_time = MAX_INVINCIBILITY_TIME
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
