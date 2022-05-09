aac263f05d8ab07f826ecd093d5ee0e8b09e9d32
Lanilor53 <lanilor53@gmail.com>
2022-05-09 19:08:46 +0300

    add dash mechanic

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,13 +1,23 @@
 extends KinematicBody2D
 
-export var SPEED = 400
-export var ROTATION_SPEED = 20
-export var REACH_ROTATION_SPEED = 3.5
 onready var look_point = get_global_mouse_position()
 var screen_size
+#speed
+export var SPEED = 400
+export var ROTATION_SPEED = 30
+export var REACH_ROTATION_SPEED = 14
+onready var velocity = Vector2.ZERO
+#health
 onready var health = 100
 export var MAX_INVINCIBILITY_TIME = 1
 onready var current_invincibility_time = 0
+# dash
+export var DASH_SPEED = 700
+export var MAX_DASH_FRAMES = 10
+export var MAX_DASH_COOLDOWN = 2
+onready var current_dash_cooldown = 0
+onready var current_dash_frames = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,21 +34,31 @@ func _physics_process(delta):
 func _process(delta):
 	if current_invincibility_time > 0:
 		current_invincibility_time -= delta
-	
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
-		 velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-	
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * SPEED
-	
-		move_and_slide(velocity)
+	if current_dash_cooldown > 0:
+		current_dash_cooldown -= delta
+	if current_dash_frames > 0:
+		current_dash_frames -= 1
+		print("dashing: %s" % velocity)
+		move_and_slide(velocity*DASH_SPEED)
+	else:
+		velocity = Vector2.ZERO # The player's movement vector.
+		if Input.is_action_pressed("move_right"):
+			 velocity.x += 1
+		if Input.is_action_pressed("move_left"):
+			velocity.x -= 1
+		if Input.is_action_pressed("move_down"):
+			velocity.y += 1
+		if Input.is_action_pressed("move_up"):
+			velocity.y -= 1
+		
+		if velocity.length() > 0:
+			velocity = velocity.normalized()
+			if Input.is_action_pressed("dash") and current_dash_cooldown <= 0:
+				move_and_slide(velocity*DASH_SPEED)
+				current_dash_cooldown = MAX_DASH_COOLDOWN
+				current_dash_frames = MAX_DASH_FRAMES
+			else:
+				move_and_slide(velocity * SPEED)
 	
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.button_index == 1:
